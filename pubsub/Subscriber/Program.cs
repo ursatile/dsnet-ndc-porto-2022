adba336aabd4c3ce0a01ebd91ed04d0aa7e37fe2
Dylan Beattoie <dylan@dylanbeattie.net>
2022-04-25 15:44:15 +0200

    Publisher/subscrier with differnet IDs

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -4,7 +4,12 @@ using Messages;
 const string AMQP = "amqps://geehsxvk:xZw6UqdzJVel3fXTPaIVKGcxWhAcp7es@sparrow.rmq.cloudamqp.com/geehsxvk";
 // See https://aka.ms/new-console-template for more information
 using var bus = RabbitHutch.CreateBus(AMQP);
-await bus.PubSub.SubscribeAsync<Greeting>("ndcporto", 
-    greeting => Console.WriteLine(greeting.Content)
-);
+await bus.PubSub.SubscribeAsync<Greeting>($"ndcporto_{Environment.MachineName}", Handle);
+
+void Handle(Greeting greeting)
+{
+    if (greeting.Content.EndsWith("5")) throw new Exception("We don't like 5!");
+    Console.WriteLine($"{greeting.Content} (from {greeting.Machine} at {greeting.Published}");
+}
+
 Console.ReadLine();
