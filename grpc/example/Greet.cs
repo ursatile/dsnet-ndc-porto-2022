f8ecc339cb99e86b3ca97fb24642a9feafe5edff
Dylan Beattoie <dylan@dylanbeattie.net>
2022-04-26 15:28:44 +0200

    Working proto example with nested messages

--- grpc/example/Greet.cs
+++ grpc/example/Greet.cs
@@ -25,14 +25,18 @@ namespace GrpcGreeter {
       byte[] descriptorData = global::System.Convert.FromBase64String(
           string.Concat(
             "CgtncmVldC5wcm90bxIFZ3JlZXQiHAoMSGVsbG9SZXF1ZXN0EgwKBG5hbWUY",
-            "ASABKAkiHQoKSGVsbG9SZXBseRIPCgdtZXNzYWdlGAEgASgJMj0KB0dyZWV0",
-            "ZXISMgoIU2F5SGVsbG8SEy5ncmVldC5IZWxsb1JlcXVlc3QaES5ncmVldC5I",
-            "ZWxsb1JlcGx5Qg6qAgtHcnBjR3JlZXRlcmIGcHJvdG8z"));
+            "ASABKAkiQAoKSGVsbG9SZXBseRIPCgdtZXNzYWdlGAEgASgJEiEKCGxhbmd1",
+            "YWdlGAIgASgLMg8uZ3JlZXQuTGFuZ3VhZ2UiWAoITGFuZ3VhZ2USDwoHaXNv",
+            "Q29kZRgBIAEoCRIRCglsb2NhbE5hbWUYAiABKAkSEwoLZW5nbGlzaE5hbWUY",
+            "AyABKAkSEwoLcmlnaHRUb2xlZnQYBCABKAgyPQoHR3JlZXRlchIyCghTYXlI",
+            "ZWxsbxITLmdyZWV0LkhlbGxvUmVxdWVzdBoRLmdyZWV0LkhlbGxvUmVwbHlC",
+            "DqoCC0dycGNHcmVldGVyYgZwcm90bzM="));
       descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
           new pbr::FileDescriptor[] { },
           new pbr::GeneratedClrTypeInfo(null, null, new pbr::GeneratedClrTypeInfo[] {
             new pbr::GeneratedClrTypeInfo(typeof(global::GrpcGreeter.HelloRequest), global::GrpcGreeter.HelloRequest.Parser, new[]{ "Name" }, null, null, null, null),
-            new pbr::GeneratedClrTypeInfo(typeof(global::GrpcGreeter.HelloReply), global::GrpcGreeter.HelloReply.Parser, new[]{ "Message" }, null, null, null, null)
+            new pbr::GeneratedClrTypeInfo(typeof(global::GrpcGreeter.HelloReply), global::GrpcGreeter.HelloReply.Parser, new[]{ "Message", "Language" }, null, null, null, null),
+            new pbr::GeneratedClrTypeInfo(typeof(global::GrpcGreeter.Language), global::GrpcGreeter.Language.Parser, new[]{ "IsoCode", "LocalName", "EnglishName", "RightToleft" }, null, null, null, null)
           }));
     }
     #endregion
@@ -269,6 +273,7 @@ namespace GrpcGreeter {
     [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
     public HelloReply(HelloReply other) : this() {
       message_ = other.message_;
+      language_ = other.language_ != null ? other.language_.Clone() : null;
       _unknownFields = pb::UnknownFieldSet.Clone(other._unknownFields);
     }
 
@@ -290,6 +295,18 @@ namespace GrpcGreeter {
       }
     }
 
+    /// <summary>Field number for the "language" field.</summary>
+    public const int LanguageFieldNumber = 2;
+    private global::GrpcGreeter.Language language_;
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public global::GrpcGreeter.Language Language {
+      get { return language_; }
+      set {
+        language_ = value;
+      }
+    }
+
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
     public override bool Equals(object other) {
@@ -306,6 +323,7 @@ namespace GrpcGreeter {
         return true;
       }
       if (Message != other.Message) return false;
+      if (!object.Equals(Language, other.Language)) return false;
       return Equals(_unknownFields, other._unknownFields);
     }
 
@@ -314,6 +332,7 @@ namespace GrpcGreeter {
     public override int GetHashCode() {
       int hash = 1;
       if (Message.Length != 0) hash ^= Message.GetHashCode();
+      if (language_ != null) hash ^= Language.GetHashCode();
       if (_unknownFields != null) {
         hash ^= _unknownFields.GetHashCode();
       }
@@ -336,6 +355,10 @@ namespace GrpcGreeter {
         output.WriteRawTag(10);
         output.WriteString(Message);
       }
+      if (language_ != null) {
+        output.WriteRawTag(18);
+        output.WriteMessage(Language);
+      }
       if (_unknownFields != null) {
         _unknownFields.WriteTo(output);
       }
@@ -350,6 +373,10 @@ namespace GrpcGreeter {
         output.WriteRawTag(10);
         output.WriteString(Message);
       }
+      if (language_ != null) {
+        output.WriteRawTag(18);
+        output.WriteMessage(Language);
+      }
       if (_unknownFields != null) {
         _unknownFields.WriteTo(ref output);
       }
@@ -362,6 +389,9 @@ namespace GrpcGreeter {
       int size = 0;
       if (Message.Length != 0) {
         size += 1 + pb::CodedOutputStream.ComputeStringSize(Message);
+      }
+      if (language_ != null) {
+        size += 1 + pb::CodedOutputStream.ComputeMessageSize(Language);
       }
       if (_unknownFields != null) {
         size += _unknownFields.CalculateSize();
@@ -377,6 +407,12 @@ namespace GrpcGreeter {
       }
       if (other.Message.Length != 0) {
         Message = other.Message;
+      }
+      if (other.language_ != null) {
+        if (language_ == null) {
+          Language = new global::GrpcGreeter.Language();
+        }
+        Language.MergeFrom(other.Language);
       }
       _unknownFields = pb::UnknownFieldSet.MergeFrom(_unknownFields, other._unknownFields);
     }
@@ -397,6 +433,13 @@ namespace GrpcGreeter {
             Message = input.ReadString();
             break;
           }
+          case 18: {
+            if (language_ == null) {
+              Language = new global::GrpcGreeter.Language();
+            }
+            input.ReadMessage(Language);
+            break;
+          }
         }
       }
     #endif
@@ -414,6 +457,313 @@ namespace GrpcGreeter {
             break;
           case 10: {
             Message = input.ReadString();
+            break;
+          }
+          case 18: {
+            if (language_ == null) {
+              Language = new global::GrpcGreeter.Language();
+            }
+            input.ReadMessage(Language);
+            break;
+          }
+        }
+      }
+    }
+    #endif
+
+  }
+
+  public sealed partial class Language : pb::IMessage<Language>
+  #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+      , pb::IBufferMessage
+  #endif
+  {
+    private static readonly pb::MessageParser<Language> _parser = new pb::MessageParser<Language>(() => new Language());
+    private pb::UnknownFieldSet _unknownFields;
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public static pb::MessageParser<Language> Parser { get { return _parser; } }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public static pbr::MessageDescriptor Descriptor {
+      get { return global::GrpcGreeter.GreetReflection.Descriptor.MessageTypes[2]; }
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    pbr::MessageDescriptor pb::IMessage.Descriptor {
+      get { return Descriptor; }
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public Language() {
+      OnConstruction();
+    }
+
+    partial void OnConstruction();
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public Language(Language other) : this() {
+      isoCode_ = other.isoCode_;
+      localName_ = other.localName_;
+      englishName_ = other.englishName_;
+      rightToleft_ = other.rightToleft_;
+      _unknownFields = pb::UnknownFieldSet.Clone(other._unknownFields);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public Language Clone() {
+      return new Language(this);
+    }
+
+    /// <summary>Field number for the "isoCode" field.</summary>
+    public const int IsoCodeFieldNumber = 1;
+    private string isoCode_ = "";
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public string IsoCode {
+      get { return isoCode_; }
+      set {
+        isoCode_ = pb::ProtoPreconditions.CheckNotNull(value, "value");
+      }
+    }
+
+    /// <summary>Field number for the "localName" field.</summary>
+    public const int LocalNameFieldNumber = 2;
+    private string localName_ = "";
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public string LocalName {
+      get { return localName_; }
+      set {
+        localName_ = pb::ProtoPreconditions.CheckNotNull(value, "value");
+      }
+    }
+
+    /// <summary>Field number for the "englishName" field.</summary>
+    public const int EnglishNameFieldNumber = 3;
+    private string englishName_ = "";
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public string EnglishName {
+      get { return englishName_; }
+      set {
+        englishName_ = pb::ProtoPreconditions.CheckNotNull(value, "value");
+      }
+    }
+
+    /// <summary>Field number for the "rightToleft" field.</summary>
+    public const int RightToleftFieldNumber = 4;
+    private bool rightToleft_;
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public bool RightToleft {
+      get { return rightToleft_; }
+      set {
+        rightToleft_ = value;
+      }
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public override bool Equals(object other) {
+      return Equals(other as Language);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public bool Equals(Language other) {
+      if (ReferenceEquals(other, null)) {
+        return false;
+      }
+      if (ReferenceEquals(other, this)) {
+        return true;
+      }
+      if (IsoCode != other.IsoCode) return false;
+      if (LocalName != other.LocalName) return false;
+      if (EnglishName != other.EnglishName) return false;
+      if (RightToleft != other.RightToleft) return false;
+      return Equals(_unknownFields, other._unknownFields);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public override int GetHashCode() {
+      int hash = 1;
+      if (IsoCode.Length != 0) hash ^= IsoCode.GetHashCode();
+      if (LocalName.Length != 0) hash ^= LocalName.GetHashCode();
+      if (EnglishName.Length != 0) hash ^= EnglishName.GetHashCode();
+      if (RightToleft != false) hash ^= RightToleft.GetHashCode();
+      if (_unknownFields != null) {
+        hash ^= _unknownFields.GetHashCode();
+      }
+      return hash;
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public override string ToString() {
+      return pb::JsonFormatter.ToDiagnosticString(this);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public void WriteTo(pb::CodedOutputStream output) {
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+      output.WriteRawMessage(this);
+    #else
+      if (IsoCode.Length != 0) {
+        output.WriteRawTag(10);
+        output.WriteString(IsoCode);
+      }
+      if (LocalName.Length != 0) {
+        output.WriteRawTag(18);
+        output.WriteString(LocalName);
+      }
+      if (EnglishName.Length != 0) {
+        output.WriteRawTag(26);
+        output.WriteString(EnglishName);
+      }
+      if (RightToleft != false) {
+        output.WriteRawTag(32);
+        output.WriteBool(RightToleft);
+      }
+      if (_unknownFields != null) {
+        _unknownFields.WriteTo(output);
+      }
+    #endif
+    }
+
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    void pb::IBufferMessage.InternalWriteTo(ref pb::WriteContext output) {
+      if (IsoCode.Length != 0) {
+        output.WriteRawTag(10);
+        output.WriteString(IsoCode);
+      }
+      if (LocalName.Length != 0) {
+        output.WriteRawTag(18);
+        output.WriteString(LocalName);
+      }
+      if (EnglishName.Length != 0) {
+        output.WriteRawTag(26);
+        output.WriteString(EnglishName);
+      }
+      if (RightToleft != false) {
+        output.WriteRawTag(32);
+        output.WriteBool(RightToleft);
+      }
+      if (_unknownFields != null) {
+        _unknownFields.WriteTo(ref output);
+      }
+    }
+    #endif
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public int CalculateSize() {
+      int size = 0;
+      if (IsoCode.Length != 0) {
+        size += 1 + pb::CodedOutputStream.ComputeStringSize(IsoCode);
+      }
+      if (LocalName.Length != 0) {
+        size += 1 + pb::CodedOutputStream.ComputeStringSize(LocalName);
+      }
+      if (EnglishName.Length != 0) {
+        size += 1 + pb::CodedOutputStream.ComputeStringSize(EnglishName);
+      }
+      if (RightToleft != false) {
+        size += 1 + 1;
+      }
+      if (_unknownFields != null) {
+        size += _unknownFields.CalculateSize();
+      }
+      return size;
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public void MergeFrom(Language other) {
+      if (other == null) {
+        return;
+      }
+      if (other.IsoCode.Length != 0) {
+        IsoCode = other.IsoCode;
+      }
+      if (other.LocalName.Length != 0) {
+        LocalName = other.LocalName;
+      }
+      if (other.EnglishName.Length != 0) {
+        EnglishName = other.EnglishName;
+      }
+      if (other.RightToleft != false) {
+        RightToleft = other.RightToleft;
+      }
+      _unknownFields = pb::UnknownFieldSet.MergeFrom(_unknownFields, other._unknownFields);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public void MergeFrom(pb::CodedInputStream input) {
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+      input.ReadRawMessage(this);
+    #else
+      uint tag;
+      while ((tag = input.ReadTag()) != 0) {
+        switch(tag) {
+          default:
+            _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, input);
+            break;
+          case 10: {
+            IsoCode = input.ReadString();
+            break;
+          }
+          case 18: {
+            LocalName = input.ReadString();
+            break;
+          }
+          case 26: {
+            EnglishName = input.ReadString();
+            break;
+          }
+          case 32: {
+            RightToleft = input.ReadBool();
+            break;
+          }
+        }
+      }
+    #endif
+    }
+
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    void pb::IBufferMessage.InternalMergeFrom(ref pb::ParseContext input) {
+      uint tag;
+      while ((tag = input.ReadTag()) != 0) {
+        switch(tag) {
+          default:
+            _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, ref input);
+            break;
+          case 10: {
+            IsoCode = input.ReadString();
+            break;
+          }
+          case 18: {
+            LocalName = input.ReadString();
+            break;
+          }
+          case 26: {
+            EnglishName = input.ReadString();
+            break;
+          }
+          case 32: {
+            RightToleft = input.ReadBool();
             break;
           }
         }
