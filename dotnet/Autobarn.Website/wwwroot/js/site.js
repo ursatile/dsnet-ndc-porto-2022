8a4f9aa8d64015f1c19bcc3616854a04f24dbb26
Dylan Beattoie <dylan@dylanbeattie.net>
2022-04-26 17:31:28 +0200

    Working styled notifications with SignalR, CSS, HTML + JS

--- dotnet/Autobarn.Website/wwwroot/js/site.js
+++ dotnet/Autobarn.Website/wwwroot/js/site.js
@@ -1,16 +1,33 @@
 ﻿$(document).ready(connectToSignalR);
 function connectToSignalR() {
     console.log("Connecting to SignalR...");
-    //window.notificationDivs = new Array();
+    window.notificationDivs = new Array();
     var conn = new signalR.HubConnectionBuilder().withUrl("/hub").build();
-    conn.on("DisplayAutobarnNotification", function (user, message) {
-        console.log(user);
-        var data = JSON.parse(message);
-        console.log(data);
-    });
+    conn.on("DisplayAutobarnNotification", displayNotification);
     conn.start().then(function () {
         console.log("SignalR has started.");
     }).catch(function (err) {
         console.log(err);
     });
+}
+function sanitize(html) {
+    return html.replace(/>/g, "&gt;").replace(/</g, "&lt;");    
+}
+function displayNotification(user, message) {
+    console.log(message);
+    let data = JSON.parse(message);
+    let $target = $("#signalr-notifications");
+    let $div = $(`<div>
+        New car notification! ${sanitize(data.manufacturer)} 
+${sanitize(data.model)}
+(${data.year}, ${sanitize(data.color)}, cost ${data.price} ${sanitize(data.currencyCode)}<br />
+<br />
+<a href="/vehicles/details/${data.registration}">Click here for more!</a>
+</div>`);
+    $target.prepend($div);
+    window.setTimeout(function () {
+        $div.fadeOut(2000, function () {
+            $div.remove();
+        });
+    }, 5000);
 }
